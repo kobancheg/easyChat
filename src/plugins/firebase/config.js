d6a09430d3c54d4dcd22f0e0171b8cf4c94c2061
Aleksey Kozhukhov <aleks.kozhukhov@MacBook-Air.local>
2020-05-03 15:22:41 +0300

    add firebase

---
+++ src/plugins/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyBqJzUQ09nwFVtKwDI0t2ZMtBswOcQtNnM',
+	authDomain: 'easy-chat-54105.firebaseapp.com',
+	databaseURL: 'https://easy-chat-54105.firebaseio.com',
+	projectId: 'easy-chat-54105',
+	storageBucket: 'easy-chat-54105.appspot.com',
+	messagingSenderId: '340480153991',
+	appId: '1:340480153991:web:919d57501401444b6b5b22',
+};
+
+export default firebaseConfig;
